cda9519221f26b23044d8c352f29cf5cbc55f883
Open Source Enthusiast <rlyunnamed@pm.me>
2025-01-31 14:48:02 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SwiftCrackMaster",
+    targets: [
+        .target(
+            name: "SwiftCrackMaster",
+            dependencies: []),
+    ]
+)
